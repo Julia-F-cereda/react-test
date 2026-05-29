7c76a4cfbd4c4b698325dded5433b150557d1435
julia cereda <julia.cereda@aluno.senai.br>
2026-05-29 08:53:21 -0300

    ADD: estamos fazendo importações e exportações em pastas para entender o react

---
+++ src/components/Alunos/Alunos.jsx
@@ -0,0 +1,14 @@
+import styles from "./alunos.module.css"
+
+function Alunos(){
+    return(
+    <>
+    <p>Nome:</p>
+    <p>Idade:</p>
+    <p>Ativo</p>
+
+    </>
+    );
+}
+
+export default Alunos
