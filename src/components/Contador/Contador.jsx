02650c7e87a52df9c4004805c5cb809022530821
julia cereda <julia.cereda@aluno.senai.br>
2026-06-12 08:54:57 -0300

    ADD: falta conseguir fazer a logica de sortear um numero aleatório

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -2,17 +2,36 @@ import { useState } from "react";
 import styles from "./Contador.module.css"
 
 function Contador(){
-    
+    const [numero, setNum] = useState(0);
+
+    // const reiniciar
+    const reiniciar = () =>{
+        setNum(0);
+    }
+
+    const somar = () =>{
+        setNum(numero + 1)
+    }
+
+    const subtrair = () =>{
+        setNum(numero - 1)
+    }
+
+    const sortear = () =>{
+
+        return Math.random() * (100 - 1) + 1;
+    }
+
     
 
     return(
         <>
-    <p className={styles.num}> 0</p>
+    <p className={styles.num}> {numero}</p>
     <div className={styles.botao}>
-        <button className={styles.reiniciar}>Reiniciar</button>
-        <button className={styles.somar}>Somar</button>
-        <button className={styles.subtrair}>Subtrair</button>
-        <button className={styles.sortear}>Sortear</button>
+        <button className={styles.reiniciar} onClick={reiniciar}>Reiniciar</button>
+        <button className={styles.somar} onClick={somar}>Somar</button>
+        <button className={styles.subtrair} onClick={subtrair}>Subtrair</button>
+        <button className={styles.sortear} onClick={sortear}>Sortear</button>
     </div>
     </>
     );
