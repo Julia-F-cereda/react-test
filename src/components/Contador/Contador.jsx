8c856e3860b7da5f34018e7e7366244c0a188849
julia cereda <julia.cereda@aluno.senai.br>
2026-06-12 14:55:43 -0300

    ADD: aparentemente o contador esta pronto e funcionando

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -22,20 +22,37 @@ function Contador(){
         setNum (Math.floor(Math.random() * (100 - 1) + 1));
     }
 
-    // const negativo = () =>{
-    //     setNum(numero < 0)
-    // }
+    const cores = () =>{
+        if(numero > 0) return styles.valorpos
+        if(numero<0) return styles.valorneg
+    }
+
+    const formatarCont = () =>{
+        if(numero >= 0 && numero < 10){
+            return `0${numero}`
+        }
+        return numero;
+    }
 
     
-
-    return(
-        <>
-    <p className={styles.num}> {numero}</p>
+// return (
+//   <li className="item">
+//     {isPacked ? name + ' ✅' : name}
+//   </li>
+// );
+// Você pode ler isso como “se isPackedfor verdadeiro, então ( ?) renderize name + ' ✅', caso contrário ( :) renderize name” .
+   return(
+    <>
+    
     <div className={styles.botao}>
+        <p className={cores()}> {formatarCont()}</p>
+        <div className={styles.bot}>
         <button className={styles.reiniciar} onClick={reiniciar}>Reiniciar</button>
         <button className={styles.somar} onClick={somar}>Somar</button>
         <button className={styles.subtrair} onClick={subtrair}>Subtrair</button>
         <button className={styles.sortear} onClick={sortear}>Sortear</button>
+        </div>
+       
     </div>
     </>
     );
